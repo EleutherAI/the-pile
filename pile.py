d44a30ce9263b5971a177aa92bc057818f706f75
Leo Gao <leogao31@gmail.com>
2020-09-04 19:32:32 -0600

    Update table formatting

--- pile.py
+++ pile.py
@@ -29,14 +29,14 @@ def mk_table(datasets):
     for dataset, weight in datasets:
         size = dataset.size()
         relative_weight = size * weight / total_weight
-        values.append([dataset.name(), size, '{:.1%}'.format(relative_weight), train_chars / size * relative_weight])
+        values.append([dataset.name(), size, '{:.2%}'.format(relative_weight), train_chars / size * relative_weight])
     
     values.sort(key=lambda x: -x[1])
     values.append(['**Total**', sum([x[1] for x in values]), "", ""])
     values = [[x[0], humanbytes(x[1]), x[2], x[3]] for x in values]
 
     writer = MarkdownTableWriter()
-    writer.table_name = "The Pile™ Components"
+    writer.table_name = "The Pile™"
     writer.headers = ["Component", "Size", "Weight", "Epochs (@1.2TB)"]
     writer.value_matrix = values
     return writer.dumps()
