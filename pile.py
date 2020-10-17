f131d41eb7b40c300639e79202229ad9505706d3
Leo Gao <leogao31@gmail.com>
2020-10-17 13:49:31 -0600

    Split size column into effective and raw

--- pile.py
+++ pile.py
@@ -119,15 +119,15 @@ def mk_table(datasets):
     for dataset, weight in datasets:
         size = dataset.size()
         relative_weight = size * weight / total_weight
-        values.append([dataset.name(), size, '{:.2%}'.format(relative_weight), train_chars / size * relative_weight, humanbytes(size / dataset.num_docs())])
+        values.append([dataset.name(), size, '{:.2%}'.format(relative_weight), train_chars / size * relative_weight, size * weight, humanbytes(size / dataset.num_docs())])
     
-    values.sort(key=lambda x: -x[1])
-    values.append(['**Total**', sum([x[1] for x in values]), "", "", humanbytes(sum([x[1] for x in values]) / sum(x[0].num_docs() for x in datasets))])
-    values = [[x[0], humanbytes(x[1]), x[2], x[3], x[4]] for x in values]
+    values.sort(key=lambda x: -x[4])
+    values.append(['**Total**', sum([x[1] for x in values]), "", "", sum([x[4] for x in values]), humanbytes(sum([x[1] for x in values]) / sum(x[0].num_docs() for x in datasets))])
+    values = [[x[0], humanbytes(x[1]), x[2], x[3], humanbytes(x[4]), x[5]] for x in values]
 
     writer = MarkdownTableWriter()
     writer.table_name = "The Pile™"
-    writer.headers = ["Component", "Size", "Weight", "Epochs", "Mean Document Size"]
+    writer.headers = ["Component", "Raw Size", "Weight", "Epochs", "Effective Size", "Mean Document Size"]
     writer.value_matrix = values
     return writer.dumps()
 
