d48210077003411614ab0f2bd8a53e6efc3f50fc
Leo Gao <leogao31@gmail.com>
2020-11-06 18:44:11 -0700

    Enable CC meta

--- the_pile/datasets.py
+++ the_pile/datasets.py
@@ -729,7 +729,7 @@ class CommonCrawlDataset(Dataset):
     def documents(self):
         self._download()
 
-        yield from lmd.Reader('components/commoncrawl/pile_cc_filtered_deduped.jsonl.zst').stream_data()
+        yield from lmd.Reader('components/commoncrawl/pile_cc_filtered_deduped.jsonl.zst').stream_data(get_meta=True)
 
     def clean(self):
         rm_if_exists('components/commoncrawl')
