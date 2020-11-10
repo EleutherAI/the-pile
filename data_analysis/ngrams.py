d06db19c14078c1b98da96d6992a4f43e4e803e1
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-11-10 21:06:42 +1000

    Update ngrams.py

--- data_analysis/ngrams.py
+++ data_analysis/ngrams.py
@@ -6,6 +6,7 @@ import tqdm
 import nltk
 from nltk.util import ngrams
 from nltk.probability import FreqDist
+from tqdm_multiprocess import TqdmMultiProcessPool
 
 from the_pile.datasets import CommonCrawlDataset
 
@@ -13,9 +14,23 @@ import logging
 from the_pile.logger import setup_logger_tqdm
 logger = logging.getLogger(__name__)
 
+# Multiprocessing
 def extract_ngrams(data, num):
-    n_grams = ngrams(nltk.word_tokenize(data), num)
-    return [ ' '.join(grams) for grams in n_grams]
+    return ngrams(nltk.word_tokenize(data), num)
+
+def process_batch(pool, batch, frequencies):
+    tasks = []
+    for document in batch:
+        task = (extract_ngrams, (document,))
+        tasks.append(task)
+
+    on_done = lambda _ : None
+    on_error = lambda _ : None
+    document_ngrams = pool.map(None, tasks, on_error, on_done)
+
+    for document_ngram in document_ngrams:
+        for n_gram in document_ngram:
+            frequencies[n_gram] += 1       
 
 def main(working_directory, process_count, n_value):
     nltk.download('punkt')
@@ -23,16 +38,25 @@ def main(working_directory, process_count, n_value):
     stats_pickle_file = os.path.join(working_directory, f"stats_n{n_value}.pkl")
 
     cc_dataset = CommonCrawlDataset()
-    document_count = cc_dataset.num_docs()
-
     frequencies = FreqDist()
 
-    with tqdm.tqdm(total=document_count, dynamic_ncols=True, unit="docs") as progress:
+    batch_size = 1000
+    batch = []
+    pool = TqdmMultiProcessPool(process_count)
+
+    with tqdm.tqdm(total=cc_dataset.num_docs(), dynamic_ncols=True, unit="docs") as progress:
         for document, meta in cc_dataset.documents():
-            n_grams = ngrams(nltk.word_tokenize(document), n_value)
-            for n_gram in n_grams:
-                frequencies[n_gram] += 1
-            progress.update()
+
+            batch.append(document)
+
+            if len(batch) == batch_size:
+                process_batch(pool, batch)
+                batch = []
+                progress.update(batch_size)
+
+        if len(batch) != 0:
+            process_batch(pool, batch)
+            progress.update(len(batch))
 
     logger.info("Pickling frequency dist")
     pickle.dump(frequencies, open(stats_pickle_file, "wb"))
@@ -40,7 +64,7 @@ def main(working_directory, process_count, n_value):
 parser = argparse.ArgumentParser(description='n-gram statistics')
 parser.add_argument("-dir", "--working_directory", default="")
 parser.add_argument("-procs", "--process_count", type=int, default=4)
-parser.add_argument("-n", "--n_value", type=int, default=4)
+parser.add_argument("-n", "--n_value", type=int, default=13)
 
 if __name__ == '__main__':
     logfile_path = "generate_minhashes.log"
